4df802a897435ea7e6a75655010440a0f208b109
medha <medhashree4930@gmail.com>
2026-03-29 00:03:04 +0530

    changes made

--- backend/firebase_config.py
+++ backend/firebase_config.py
@@ -1,8 +1,13 @@
 import firebase_admin
 from firebase_admin import credentials, firestore
+import os
+import json
+
+firebase_config = json.loads(os.getenv("FIREBASE_CONFIG"))
+
 
 if not firebase_admin._apps:
-    cred = credentials.Certificate("firebase_key.json")
+    cred = credentials.Certificate(firebase_config)
     firebase_admin.initialize_app(cred)
 
 db = firestore.client()
